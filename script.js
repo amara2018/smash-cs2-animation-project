3678493a6b1f99dc6e7fe527600dd2d7540fdcfc
amara2018 <40439209+amara2018@users.noreply.github.com>
2018-06-29 13:57:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-// Global Variables
+/// Global Variables
 var p5Canvas;
 var myName;
+var rectX;
+var rectY;
+
 
 // initialize global variables in setup() function
 function setup() {
   p5Canvas = createCanvas(800, 600);
   p5Canvas.parent("#p5-canvas");
   myName = select("#my-name");
-  myName.html("[Your Name Here]");
-}
+  myName.html("Amara");
 
+    
+  }
 
 function draw() {
   background(0, 255, 0);
+      drawCircle () ;
+    variableEllipse(mouseX, mouseY, pmouseX, pmouseY);  
+    drawCirclething () ;
+   
+}
+                    
+
+function drawCircle() {
+    fill("orange");
+    ellipse(56, 46, 55, 55);
+}
+function variableEllipse(x, y, px, py) {
+    var speed = abs(x-px) + 60 * abs(y-py);
+    stroke(speed); 
+    fill("orange");
+    ellipse(x, y, speed, speed);
+        
 }
 
-// create an animation function below, then call it in draw()
-// see example.js if you need a place to start!
-// when you're ready, be sure to change index.html to link to this script instead of example.js
+ 
+
+function drawCirclething () {
+     fill('#fae');
+    ellipse(100, 100, 85, 85);
+}
+
+
+
+
